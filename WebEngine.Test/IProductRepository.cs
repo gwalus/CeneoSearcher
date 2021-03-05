321753b8584580385eea96e2356ffef3b47a0168
Grzegorz Waluś <gwalus1404@gmail.com>
2021-03-05 14:27:26 +0100

    Create product repository

---
+++ WebEngine.Test/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace WebEngine
+{
+    public interface IProductRepository
+    {
+        ICollection<Product> GetAll();
+    }
+}
---
+++ WebEngine.Test/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebEngine
+{
+    class ProductRepository : IProductRepository
+    {
+        public ICollection<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
