ef4fe0239909ab114b4665ea684aee5693f14937
Grzegorz Waluś <gwalus1404@gmail.com>
2021-03-05 14:32:14 +0100

    Create ShouldReturnAllProducts

---
+++ WebEngine.Test/CeneoControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace WebEngine
+{
+    public class CeneoControllerTests
+    {
+        private IList<Product> _products;
+
+        [Fact]
+        public void ShouldReturnAllProducts()
+        {
+            //Arange
+            var options = new DbContextOptionsBuilder<ProductContext>()
+                .UseInMemoryDatabase(databaseName: "ShouldGetAllProducts")
+                .Options;
+
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            _products = fixture.Create<IList<Product>>();
+
+            using (var context = new ProductContext(options))
+            {
+                foreach (var product in _products)
+                {
+                    context.Add(product);
+                    context.SaveChanges();
+                }
+            }
+
+            //Act
+            IList<Product> actualProducts;
+            using (var context = new ProductContext(options))
+            {
+                var repository = new ProductRepository(context);
+                actualProducts = repository.GetAll().ToList();
+            }
+
+            //Assert
+            Assert.Equal(_products.Count, actualProducts.Count);
+        }
+    }
+}
--- WebEngine.Test/ProductRepository.cs
+++ WebEngine.Test/ProductRepository.cs
@@ -5,6 +5,13 @@ namespace WebEngine
 {
     class ProductRepository : IProductRepository
     {
+        private ProductContext _context;
+
+        public ProductRepository(ProductContext context)
+        {
+            _context = context;
+        }
+
         public ICollection<Product> GetAll()
         {
             throw new NotImplementedException();
