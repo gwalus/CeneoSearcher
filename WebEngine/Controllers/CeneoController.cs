3ce55cb5fcc4bd2303f8f350919ca2630feb5deb
Grzegorz Waluś <gwalus1404@gmail.com>
2021-03-05 14:20:14 +0100

    Clear project to default

--- WebEngine/Controllers/CeneoController.cs
+++ WebEngine/Controllers/CeneoController.cs
@@ -1,20 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-
-namespace WebEngine.Controllers
+﻿namespace WebEngine.Controllers
 {
     public class CeneoController : BaseApiController
-    {
-        [HttpGet]
-        public List<Person> GetPeople()
-        {
-            return new List<Person>() { new Person() { Id = 1, Name = "Hugo" }, new Person() { Id = 2, Name = "Kopter" } };
-        }
-    }
-
-    public class Person
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
+    {       
+        
     }
 }
