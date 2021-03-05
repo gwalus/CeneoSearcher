7ee8337fe48e5c903284a447dabf9de2eb7c73b4
Grzegorz Waluś <gwalus1404@gmail.com>
2021-03-05 14:25:08 +0100

    Add product model

---
+++ WebEngine.Test/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebEngine
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        [Key]
+        public string Link { get; set; }
+        public string Image { get; set; }
+        public string Rate { get; set; }
+        public string Price { get; set; }
+    }
+}
