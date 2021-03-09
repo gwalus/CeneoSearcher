5c89a337062c54dcf2349ecc0ba3d2016364beaf
Jakub Gawęda <kubag98@hotmail.com>
2021-03-09 15:16:01 +0100

    add Offers class

---
+++ WebEngine/Model/Offers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebEngine.Model
+{
+    public class Offers
+    {
+        public double LowPrice { get; set; }
+    }
+}
