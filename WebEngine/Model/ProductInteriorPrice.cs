1f2011b633e7353dd93235144d7cb3d5b598c63b
Jakub Gawęda <kubag98@hotmail.com>
2021-03-09 15:17:04 +0100

    add ProductInteriorPrice class

---
+++ WebEngine/Model/ProductInteriorPrice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebEngine.Model
+{
+    public class ProductInteriorPrice
+    {
+        public Offers Offers { get; set; }
+    }
+}
