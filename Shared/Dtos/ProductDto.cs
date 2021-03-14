d7aa218b713b9534857353d85a679a01f722df5b
Grzegorz Waluś <gwalus1404@gmail.com>
2021-03-14 15:20:28 +0100

    Add product dto in shared project

---
+++ Shared/Dtos/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Dtos
+{
+    public class ProductDto
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+        public string Image { get; set; }
+        public string Rate { get; set; }
+        public string Price { get; set; }
+        public bool IsSubscribed { get; set; }
+    }
+}
